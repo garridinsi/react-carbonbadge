8f5cb1b26cbcc263244b35f4348a0167f1732339
garridinsi <info@enekogarrido.com>
2022-09-26 08:47:15 +0200

    before upgrade

--- src/carbonbadge.js
+++ src/carbonbadge.js
@@ -1,22 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
-const Carbonbadge = props => {
+const Carbonbadge = ({ darkMode }) => {
   useEffect(() => {
-    const script = document.createElement("script")
+    const script = document.createElement("script");
 
-    script.src = "https://unpkg.com/website-carbon-badges@^1/b.min.js"
-    script.defer = true
+    script.src = "https://unpkg.com/website-carbon-badges@^1/b.min.js";
+    script.defer = true;
 
-    document.body.appendChild(script)
+    document.body.appendChild(script);
 
     return () => {
-      document.body.removeChild(script)
-    }
-  }, [])
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
-    <div id="wcb" className={`wcb carbonbadge${props.darkMode ? ` wcb-d` : '' }`}></div>
-  )
-}
+    <div
+      id="wcb"
+      className={`wcb carbonbadge${darkMode ? ` wcb-d` : ""}`}
+    ></div>
+  );
+};
 
-export default Carbonbadge
+export default Carbonbadge;
