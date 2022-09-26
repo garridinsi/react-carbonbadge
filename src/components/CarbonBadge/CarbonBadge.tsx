c7948a6422d629c09efdca88dfb3f1baf5e3820a
garridinsi <info@enekogarrido.com>
2022-09-27 00:43:53 +0200

    ya ns

--- src/components/CarbonBadge/CarbonBadge.tsx
+++ src/components/CarbonBadge/CarbonBadge.tsx
@@ -1,33 +1,18 @@
 import React, { useEffect } from "react";
+import "https://unpkg.com/website-carbon-badges@1.1.3/b.min.js";
 
 export interface CarbonBadgeProps {
   darkMode: boolean;
 }
 
-function useScript() {
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = "https://unpkg.com/website-carbon-badges@^1/b.min.js";
-    script.defer = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-}
-
 const CarbonBadge = (props: CarbonBadgeProps) => {
   return (
-    <div>
+    <>
       <div
         id="wcb"
         className={`wcb carbonbadge${props.darkMode ? ` wcb-d` : ""}`}
       ></div>
-      {useScript()}
-    </div>
+    </>
   );
 };
 
